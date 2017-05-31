1a6830a8d64fd1c05fbb9990c643e657bb3d526e
iherewy <85642319@qq.com>
2017-05-31 23:43:30 +0800

    info edit

--- resources/views/personal/info-edit.blade.php
+++ resources/views/personal/info-edit.blade.php
@@ -29,7 +29,7 @@
                 <div class="ui-list-info">
                     <h4>昵称</h4>
                 </div>
-                <div class="ui-list-action">I'm here.</div>
+                <div class="ui-list-action" id="name">I'm here.</div>
             </li>
         </div>
         <div class="ui-list ui-list-text ui-border-b">
@@ -37,16 +37,16 @@
                 <div class="ui-list-info">
                     <h4>手机号</h4>
                 </div>
-                <div class="ui-list-action">13387525826</div>
+                <div class="ui-list-action" id="phone">13387525826</div>
             </li>
         </div>
         <div class="ui-list ui-list-text ui-border-b ui-list-link">
-            <li class="ui-border-t">
+            <li class="ui-border-t" onclick="infoEdit(name_html,this,'name')">
                 <h4 class="ui-nowrap">真实姓名</h4>
                 <div class="ui-txt-info">李四</div>
             </li>
         </div>
-        <div class="ui-list ui-list-text ui-border-b ui-list-link">
+        <div class="ui-list ui-list-text ui-border-b ui-list-link" onclick="infoEdit(sex_html,this,'sex')">
             <li class="ui-border-t">
                 <h4 class="ui-nowrap">性别</h4>
                 <div class="ui-txt-info">男</div>
@@ -58,7 +58,7 @@
                 <div class="ui-txt-info">1995-3-10</div>
             </li>
         </div>
-        <div class="ui-list ui-list-text ui-border-b ui-list-link">
+        <div class="ui-list ui-list-text ui-border-b ui-list-link" onclick="infoEdit(email_html,this,'email')">
             <li class="ui-border-t">
                 <h4 class="ui-nowrap">电子邮箱</h4>
                 <div class="ui-txt-info">85642319@qq.com</div>
@@ -71,5 +71,116 @@
             </li>
         </div>
     </div>
+    <script src="{{asset('/js/zepto.min.js')}}"></script>
+    <script src="{{asset('/js/layer.js')}}"></script>
+    <script>
+        function postForm(url,data,myFuc) {
+            $.ajax({
+                type: "post",
+                data: data,
+                url: url,
+                dataType: "json",
+                success: function(res){
+                    if(res.code == 200) {
+                        layer.open({
+                            content: res.message
+                            ,skin: 'msg'
+                            ,time: 2
+                        });
+                        myFuc();
+                    } else {
+                        layer.open({
+                            content: res.message
+                            ,skin: 'msg'
+                            ,time: 2
+                        });
+                    }
+                }
+            });
+        }
+        var name_html = '<div class="ui-form">'+
+                            '<div class="ui-form-item ui-form-item-pure ui-border-radius ui-form">'+
+                                '<input type="text" placeholder="请输入姓名" name="name" id="real_name" value="吴越">'+
+                            '</div>'+
+                        '</div>'
+        var sex_html = '<div class="ui-form">'+
+                            '<ul class="ui-list ui-list-text" id="sex">'+
+                                '<div class="text-left m-top">'+
+                                    '<label class="ui-radio">'+
+                                        '<input type="radio" checked name="sex" value="男">'+
+                                    '</label>'+
+                                    '<span>男</span>'+
+                                '</div>'+
+                                '<div class="text-left m-top">'+
+                                    '<label class="ui-radio">'+
+                                        '<input type="radio" name="sex" value="女">'+
+                                    '</label>'+
+                                    '<span>女</span>'+
+                                '</div>'+
+                            '</ul>'+
+                        '</div>'
+        var email_html = '<div class="ui-form">'+
+                             '<div class="ui-form-item ui-form-item-pure ui-border-radius ui-form">'+
+                                 '<input type="text" placeholder="请输入邮箱" name="name" id="email" value="85642319@qq.com">'+
+                             '</div>'+
+                         '</div>'
+        function infoEdit(html,el,type) {
+            var name = document.getElementById('name').innerText;
+            var phone = document.getElementById('phone').innerText;
+            layer.open({
+                content: html
+                ,className:'apply'
+                ,btn: ['保存', '取消']
+                ,yes: function(index){
+                    var data = {
+                        name:name,
+                        phone:phone,
+                    };
+                    switch (type){
+                        case 'name':
+                            var real_name = document.getElementById('real_name').value;
+                            data.read_name = real_name;
+                            console.log(data)
+                            if (real_name.length == 0) {
+                                layer.open({
+                                    content: "姓名不能为空！"
+                                    ,skin: 'msg'
+                                    ,time: 2
+                                });
+                            }else {
+                                postForm('/personal/info-edit',data,function () {
+                                    $(el).find('.ui-txt-info').html(real_name);
+                                })
+                            }
+                            break;
+                        case 'sex':
+                            var sex = $('#sex').find("input[name='sex']:checked").val();
+                            data.sex = sex;
+                            console.log(data)
+                            postForm('/personal/info-edit',data,function () {
+                                $(el).find('.ui-txt-info').html(sex);
+                            })
+                            break;
+                        case 'email':
+                            var email = document.getElementById('email').value;
+                            data.email = email;
+                            console.log(data)
+                            if (email.length == 0) {
+                                layer.open({
+                                    content: "邮箱不能为空！"
+                                    ,skin: 'msg'
+                                    ,time: 2
+                                });
+                            }else {
+                                postForm('/personal/info-edit',data,function () {
+                                    $(el).find('.ui-txt-info').html(email);
+                                })
+                            }
+                            break;
+                    }
+                }
+            });
+        }
+    </script>
 </body>
 </html>
