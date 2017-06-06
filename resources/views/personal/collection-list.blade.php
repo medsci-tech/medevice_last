18cbf950017a095dff04c47a24f0b4f72a8e09df
iherewy <85642319@qq.com>
2017-06-06 19:01:08 +0800

    fix

--- resources/views/personal/collection-list.blade.php
+++ resources/views/personal/collection-list.blade.php
@@ -6,91 +6,71 @@
     <meta name="format-detection" content="telephone=no">
     <title>我的收藏</title>
     <link rel="stylesheet" href="{{asset('/css/frozen.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
 </head>
 <body>
-<div>
-    @foreach($collections as $collection)
-        <ul class="ui-list  ui-border-tb" style="margin-top: 10px">
-            <li class="ui-border-t">
-                <div class="ui-list-info">
-                    <h5>{{$collection->product->name}}</h5>
-                </div>
-            </li>
-            <li class="ui-border-t">
-                <div class="ui-list-thumb">
-                    <span style="background-image:url({{$collection->product->logo}}?imageView2/1/w/65/h/65/q/90)"></span>
-                </div>
-                <div class="ui-list-info">
-                    <a href="/shop/detail?id={{$collection->product->id}}">
-                        <h5 class="ui-nowrap">{{$collection->product->name}}</h5>
-
-                        <p class="ui-nowrap ui-txt-info">时间：{{$collection->created_at}}</p>
-                    </a>
-                </div>
-            </li>
-            <li class="ui-border-t">
-                <div class="ui-list-info">
-                    <h5>价格：<span class="ui-txt-warning">{{$collection->product->price}}</span></h5>
-                    {{--<h5>价格：<span class="ui-txt-warning">￥{{$collection->product->price}}</span></h5>--}}
-                </div>
-                <div class="order_pay"><h6 class="ui-list-action ui-btn ui-btn-danger"
-                                           onclick="cancelCollect({{$collection->product->id}})">取消收藏</h6></div>
-            </li>
-        </ul>
-    @endforeach
-</div>
-<div class="ui-txt-tips ui-txt-info ui-flex ui-flex-pack-center ui-top">已经没有更多收藏了！</div>
-<div class="ui-dialog" id="dialog">
-    <div class="ui-dialog-cnt">
-        <header class="ui-dialog-hd ui-border-b">
-            <h3 id="dia_title"></h3>
-            <i class="ui-dialog-close" data-role="button" onclick="closeDia()"></i>
-        </header>
-        <div class="ui-dialog-bd">
-            <i class="ui-icon-success success_dia" id="icon"></i><h4 id="dia_content"></h4>
-        </div>
-        <div class="ui-dialog-ft">
-            <button type="button" data-role="button" onclick="closeDia()">确定</button>
-        </div>
+<header class="head-bar">
+    <div class="bar-left">
+        <a href="javascript:history.back()" class="goback"></a>
     </div>
-</div>
+    <div class="head-title">我的收藏</div>
+</header>
+<section class="ui-panel ui-panel-pure ui-border-t cooperate mark-box">
+    <ul class="ui-list ui-border-tb">
+        <li class="ui-border-t">
+            <div class="mark-info">
+                <div class="ui-list-img">
+                    <span style="background-image:url(http://placeholder.qiniudn.com/200x200)"></span>
+                </div>
+                <div class="ui-list-info">
+                    <h5 class="ui-txt-sub ui-txt-default">这是标题这是标题这是标题这是标题这是标题这是标题这是标题</h5>
+                </div>
+            </div>
+            <div class="del ui-border-l" onclick="cancelCollect(这里商品id,this)">取消收藏</div>
+        </li>
+        <li class="ui-border-t">
+            <div class="mark-info">
+                <div class="ui-list-img">
+                    <span style="background-image:url(http://placeholder.qiniudn.com/200x200)"></span>
+                </div>
+                <div class="ui-list-info">
+                    <h5 class="ui-txt-sub ui-txt-default">这是标题这是标题这是标题这是标题这是标题这是标题这是标题</h5>
+                </div>
+            </div>
+            <div class="del ui-border-l" onclick="cancelCollect(这里商品id,this)">取消收藏</div>
+        </li>
+    </ul>
+</section>
 
-<script src="http://cdn.bootcss.com/bootswatch/2.0.2/js/jquery.js"></script>
+<script src="{{asset('/js/zepto.min.js')}}"></script>
+<script src="{{asset('/js/layer.js')}}"></script>
 <script>
-    function showDia(success, title, content) {
-        $("#dia_title").text(title);
-        $("#dia_content").text(content);
-        if (success) {
-            $("#icon").removeClass().addClass("ui-icon-success success_dia");
-        } else {
-            $("#icon").removeClass().addClass("ui-icon-success ui-txt-warning");
-        }
-        document.getElementById("dialog").style.display = "-webkit-box";
-    }
-    function closeDia() {
-        window.location.reload();
-        document.getElementById("dialog").style.display = "none";
-    }
-
-    function cancelCollect(product_id) {
+    function cancelCollect(product_id,el) {
         $.ajax({
-            url: '/shop/cancel-collect',
+            url: '/shop/collect',
             data: {
-                product_id: product_id
+                product_id: product_id,
+                action:0
             },
-            type: "get",
+            type: "POST",
             dataType: "json",
             success: function (json) {
-                if (json.success) {
-                    showDia(true, '取消成功', '已取消收藏！');
-                } else {
-                    showDia(true, '取消失败', '取消收藏失败,请重试！');
+                if (json.code == 200) {
+                    layer.open({
+                        content: json.message
+                        , skin: 'msg'
+                        , time: 2
+                    });
+                    $(el).parent().remove();
                 }
             },
             error: function (xhr, status, errorThrown) {
                 console.log("Sorry, there was a problem!");
-                showDia(true, '关注失败', '关注失败,请重试！');
+                layer.open({
+                    content: "取消收藏失败！"
+                    , skin: 'msg'
+                    , time: 2
+                });
             }
         });
     }
